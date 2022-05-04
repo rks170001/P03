e7ec97bb494ce96e2d86302d4554f1aba8610cad
rks170001 <rks170001@utdallas.edu>
2022-05-04 17:05:16 -0500

    Project 03

---
+++ Project03/Assets/Scripts/LevelController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitLevel();
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            ResetLevel();
+        }
+
+
+        void ExitLevel()
+        {
+
+            Application.Quit();
+        }
+
+        void ResetLevel()
+        {
+            SceneManager.LoadScene("Scene01");
+        }
+
+
+
+    }
+}
---
+++ Project03/Assets/Scripts/LootChest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootChest : MonoBehaviour
+{
+    [SerializeField] ParticleSystem lootParticle;
+    private Animator _anim;
+    public AudioClip ChestSound;
+    public AudioSource audioSource;
+    public bool alreadyPlayed = false;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            
+            OpenChest();
+            audioSource.PlayOneShot(ChestSound);
+        }
+    }
+
+    public void OpenChest()
+    {
+        
+        _anim.SetTrigger("Open");
+        Invoke("lootEffect", 2.8f);
+        
+
+    }
+
+    private void lootEffect()
+    {
+        lootParticle.Play();
+    }
+    
+}
